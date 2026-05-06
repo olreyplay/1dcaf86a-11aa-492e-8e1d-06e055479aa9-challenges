529e6eef7c651290c7c6ab9c42695a2ede5a8122
Oleh Subotin <126164012+olreyplay@users.noreply.github.com>
2026-05-06 14:54:06 +0300

    3

---
+++ challenge-03-solution-show-user-status-messages/src/App.jsx
@@ -0,0 +1,23 @@
+// Change the code below this line.
+const StatusCard = (props) => {
+  return (
+    <>
+      <h2>{props.name}</h2>
+
+      <p>{props.online ? "Online" : "Offline"}</p>
+    </>
+  );
+};
+// Change the code above this line.
+
+const App = () => {
+  return (
+    <>
+      <StatusCard name="Emma Wilson" online={true} />
+      <StatusCard name="Daniel Lee" online={false} />
+      <StatusCard name="Sophia Turner" online={true} />
+    </>
+  );
+};
+
+export default App;
