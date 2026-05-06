08200b6d8d9eb217e213ef7a7e48d7f36c2d1e64
Oleh Subotin <126164012+olreyplay@users.noreply.github.com>
2026-05-06 12:23:07 +0300

    clean sols

---
+++ challenge-06-solution/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <h1>solution 01</h1>
+      <p>
+        This is a solution to create a simple web page with a header and a
+        paragraph.
+      </p>
+    </>
+  );
+};
+
+export default App;
