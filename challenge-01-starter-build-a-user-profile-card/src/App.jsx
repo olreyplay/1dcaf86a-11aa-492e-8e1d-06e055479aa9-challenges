6eb272f90631a05fb43ea312ae1279a34c9cda56
Oleh Subotin <126164012+olreyplay@users.noreply.github.com>
2026-05-06 13:22:36 +0300

    ch-1

---
+++ challenge-01-starter-build-a-user-profile-card/src/App.jsx
@@ -0,0 +1,17 @@
+const avatarUrl =
+  "https://codefinity-content-media-v2.s3.eu-west-1.amazonaws.com/courses/1dcaf86a-11aa-492e-8e1d-06e055479aa9/9ea7774a-16ac-4122-9a8b-a9a9d105e7c5/boy.png";
+
+// Change the code below this line.
+const App = () => {
+  return (
+    <>
+      <img src={___} alt="___" width={___} />
+      <___>___</___>
+      <___>___</___>
+      <___>___</___>
+    </>
+  );
+};
+// Change the code above this line.
+
+export default App;
